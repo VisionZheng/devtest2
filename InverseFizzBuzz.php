5eb0be011e9345566593616d9c4c1b626704c65b
Devtest TestButler <testbutler@devtest.net>
2017-07-06 08:19:46 +0000

    Attempt 1

--- InverseFizzBuzz.php
+++ InverseFizzBuzz.php
@@ -5,7 +5,95 @@ class InverseFizzBuzz {
     }
     
     public function sequence() {
-        return array(3);
+
+
+        $resultList = array();
+        for($i = 1; $i <= 100; $i++){
+            
+            
+            
+            $index = $i;
+            $count = count($this->list);
+            $num = 0;
+            $resultHash = array();
+            $result = array();
+            
+            foreach($this->list as $key => $val) {
+              
+                if($index % 15 == 0) {
+                    $now = 'fizzbuzz';
+                } elseif($index % 3 == 0) {
+                    $now = 'fizz';
+                } elseif($index % 5 == 0) {
+                    $now = 'buzz';
+                } else {
+                    break;
+                }
+                
+                if(strpos($now, $val) !== false) {
+                    // echo $index . '   ';
+                    array_push($resultHash, $index);
+                    if($index != 100) {
+                        $index = $this->find_next($index);
+                        $num++;
+                    }
+                }
+                    
+            }
+            // echo "<br />";
+
+
+
+            
+            if ($resultHash && ($num == $count)){
+                // echo "<pre>";
+                // print_r($resultHash);
+                $time = count($resultHash) - 1;
+                for($j = $resultHash[0]; $j <= $resultHash[$time]; $j++){
+                    array_push($result, $j);
+                }
+
+                array_push($resultList, $result);
+                
+            }
+            
+        }
+        
+        
+        
+        if (count($resultList) == 0){
+            return null;
+        }
+
+
+        $count = 100;
+        foreach ($resultList as $v){
+            $length = count($v);
+            if ($length < $count){
+                $count = $length;
+                $result = $v;
+            }
+        }
+
+        // echo "<pre>";
+        // print_r($result);
+        if($result) {
+            return $result;
+        }
+        return null;
+        
+        
+    }
+    
+    
+    public function find_next($i) {
+        for($i = $i + 1; $i <= 100; $i++){
+ 
+            if($i % 5 == 0 || $i % 3 == 0){
+                return $i;
+            }
+        }
+
     }
 }
 ?>
