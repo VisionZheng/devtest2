bd76e836934b19025e774c398e8a403506d40188
Devtest TestButler <testbutler@devtest.net>
2017-07-06 08:19:27 +0000

    Initial Commit

---
+++ InverseFizzBuzz.php
@@ -0,0 +1,11 @@
+<?php        
+class InverseFizzBuzz {
+    public function __construct($list) {
+        $this->list = $list;
+    }
+    
+    public function sequence() {
+        return array(3);
+    }
+}
+?>
